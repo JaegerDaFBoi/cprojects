429cf68de2708b89fb4a95e99cc025d362b2700e
Juan Morales <mcallister2696@gmail.com>
2021-11-04 21:38:11 -0500

    creacion del app context

--- Ejercicio.App.Dominio/Entidades/Paciente.cs
+++ Ejercicio.App.Dominio/Entidades/Paciente.cs
@@ -8,6 +8,6 @@ namespace Ejercicio.App.Dominio
         public string Ciudad { get; set; }
         public DateTime FechaNacimiento { get; set; }
         public Medico Medico { get; set; }
-        
+        public List<SignoVital> SignoVitales { get; set; }
     }
 }
